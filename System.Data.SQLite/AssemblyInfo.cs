9d5221419e871b5c306b62849c1f663b26f5ef0b
mistachkin <mistachkin>
2017-06-10 17:44:34 +0000

    Bump all versions for the 1.0.105.2 release.

--- System.Data.SQLite/AssemblyInfo.cs
+++ System.Data.SQLite/AssemblyInfo.cs
@@ -72,7 +72,7 @@ using System.Runtime.ConstrainedExecution;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.105.1")]
+[assembly: AssemblyVersion("1.0.105.2")]
 #if !PLATFORM_COMPACTFRAMEWORK
-[assembly: AssemblyFileVersion("1.0.105.1")]
+[assembly: AssemblyFileVersion("1.0.105.2")]
 #endif
